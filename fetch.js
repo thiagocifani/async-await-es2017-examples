4cd21f974a6255352c02ce0257286d0381585740
Thiago Cifani <cifani.thiago@gmail.com>
2017-05-04 14:27:11 +0700

    Adds fetch.js example. Promise without async and await.

---
+++ fetch.js
@@ -0,0 +1,14 @@
+const fetch = require('node-fetch');
+
+function showGitHubUser(handle) {
+  const url = `https://api.github.com/users/${handle}`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(user => {
+      console.log(user.name);
+      console.log(user.location);
+    });
+}
+
+showGitHubUser('thiagocifani');
